b078fa5d6617cfbff3e8bcc5a4fb669411afcf15
min3 <92773767+imai3@users.noreply.github.com>
2023-03-19 14:21:32 +0000

    特定の色の上にしか描けない機能

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let color = [150, 255];
 let line = [1, 3]
 let num = 0
 const ten_haba = 30
+let only_color = 999
 context.strokeStyle = `rgb(${color[num]},${color[num]},${color[num]})`
 context.lineWidth = line[num]
 
@@ -27,6 +28,7 @@ window.addEventListener("keydown", (event) => {
 	if (event.key == "5") line[num] -= 1
 	if (line[num] > 55) line[num] = 55
 	if (line[num] < 0) line[num] = 0
+	if (event.key == "0") penmode = "draw_only"
 
 	context.strokeStyle = `rgb(${color[num]},${color[num]},${color[num]})`
 	context.lineWidth = line[num]
@@ -35,6 +37,7 @@ window.addEventListener("keydown", (event) => {
 window.addEventListener("keyup", (event) => {
 	if (event.key == "Enter") penmode = "move";
 	if (event.key == ".") penmode = "move";
+	if (event.key == "0") penmode = "move";
 	if (event.key == "+") num = (num + 1) % 2;
 })
 
@@ -43,7 +46,18 @@ canvas.addEventListener('touchstart', (event) => {
 	const rect = canvas.getBoundingClientRect();
 	lastX = event.touches[0].clientX - rect.left;
 	lastY = event.touches[0].clientY - rect.top;
+	const imageData = context.getImageData(lastX, lastY, 1, 1);
+	if (penmode == "pick") color[num] = imageData.data[0];
+	if (penmode == "draw_only") only_color = imageData.data[0];
 });
+
+
+function draw_pixel(x, y, color) {
+	context.beginPath();
+	context.rect(x, y, 1, 1)
+	context.fillStyle = `rgb(${color},${color},${color})`;
+	context.fill();
+}
 
 canvas.addEventListener('touchmove', (event) => {
 	if (penmode == "move") return
@@ -55,10 +69,7 @@ canvas.addEventListener('touchmove', (event) => {
 		const dx = (x - lastX) / ten_haba
 		const dy = (y - lastY) / ten_haba
 		for (let i = 0; i < ten_haba; i++) {
-			context.beginPath();
-			context.rect(lastX + dx * i, lastY + dy * i, 1, 1)
-			context.fillStyle = `rgb(${color[num]},${color[num]},${color[num]})`;
-			context.fill();
+			draw_pixel(lastX + dx * i, lastY + dy * i, color[num])
 		}
 		lastX = x;
 		lastY = y;
@@ -70,13 +81,22 @@ canvas.addEventListener('touchmove', (event) => {
 		const imageData = context.getImageData(x, y, 1, 1);
 		color[num] = imageData.data[0]
 	}
+	if (touching && penmode == "draw_only") {
+		const rect = canvas.getBoundingClientRect();
+		const x = event.touches[0].clientX - rect.left;
+		const y = event.touches[0].clientY - rect.top;
+		const dx = (x - lastX) / ten_haba
+		const dy = (y - lastY) / ten_haba
+		for (let i = 0; i < ten_haba; i++) {
+			if (only_color == context.getImageData(lastX + dx * i, lastY + dy * i, 1, 1).data[0]) draw_pixel(lastX + dx * i, lastY + dy * i, color[num])
+		}
+		lastX = x;
+		lastY = y;
+	}
 
 });
 
 
-setInterval(function() {
-
-})
 
 canvas.addEventListener('touchend', () => {
 	touching = false;
